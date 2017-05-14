0f32231b9ac0b6d487b31eb129db8a1ba8081b10
FoKSS37 <maks_morkrov@mail.ru>
2017-05-14 23:23:19 +0300

    Добавлен код в Hubs

--- ChatProject/Hubs/CHub.cs
+++ ChatProject/Hubs/CHub.cs
@@ -12,6 +12,8 @@ namespace ChatProject.Hubs
 {
     class CHub : Hub
     {
+        static List<Client> Users = new List<Client>();
+
         /// <summary>
         /// Отправить сообщение
         /// </summary>
@@ -19,7 +21,7 @@ namespace ChatProject.Hubs
         /// <param name="_mess">Текст сообщения</param>
         public void SMess(string _name, string _mess)
         {
-
+            Clients.All.addMessage(_name, _mess);
         }
         /// <summary>
         /// Установить соединение с приложением
@@ -27,16 +29,35 @@ namespace ChatProject.Hubs
         /// <param name="_name">Имя пользователя.</param>
         public void Connect(string _name)
         {
+            var id = Context.ConnectionId;
 
+
+            if (!Users.Any(x => x.ID == id))
+            {
+                Users.Add(new Client { ID = id, Login = _name });
+
+                // Посылаем сообщение текущему пользователю
+                Clients.Caller.onConnected(id, _name, Users);
+
+                // Посылаем сообщение всем пользователям, кроме текущего
+                Clients.AllExcept(id).onNewUserConnected(id, _name);
+            }
         }
 
         /// <summary>
         /// Отключение пользователя
         /// </summary>
-        public void Stop()
+        public override System.Threading.Tasks.Task OnDisconnected(bool stopCalled)
         {
+            var item = Users.FirstOrDefault(x => x.ID == Context.ConnectionId);
+            if (item != null)
+            {
+                Users.Remove(item);
+                var id = Context.ConnectionId;
+                Clients.All.onUserDisconnected(id, item.Login);
+            }
 
+            return base.OnDisconnected(stopCalled);
         }
-
     }
 }
