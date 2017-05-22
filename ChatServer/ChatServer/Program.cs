125675ff57213686029584d5a636a91357514406
PavelFun <pashagun37@gmail.com>
2017-05-22 18:06:17 +0300

    Пересобрал консольные ChatClient и ChatServer проекты. Теперь работоспособен.

--- ChatServer/ChatServer/Program.cs
+++ ChatServer/ChatServer/Program.cs
@@ -1,15 +1,25 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 
 namespace ChatServer
 {
     class Program
     {
+        static ServerObject server; // сервер
+        static Thread listenThread; // потока для прослушивания
         static void Main(string[] args)
         {
+            try
+            {
+                server = new ServerObject();
+                listenThread = new Thread(new ThreadStart(server.Listen));
+                listenThread.Start(); //старт потока
+            }
+            catch (Exception ex)
+            {
+                server.Disconnect();
+                Console.WriteLine(ex.Message);
+            }
         }
     }
 }
